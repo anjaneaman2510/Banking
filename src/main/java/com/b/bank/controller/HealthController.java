b726926fe92b168b45090201690013b565e411b5
amananjane2510 <anjaneaman2510@gmail.com>
2026-09-14 23:11:56 +0530

    updat96

--- src/main/java/com/b/bank/controller/HealthController.java
+++ src/main/java/com/b/bank/controller/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin 
-@Controller 
+@RestController 
 public class HealthController {
         @GetMapping("/health")
       public ResponseEntity<String> healthCHeck()
